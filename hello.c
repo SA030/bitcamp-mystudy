7627c22f225ee0baa292fb21d21bd3064abc1635
SA030 <suna_0132@naver.com>
2024-05-28 16:00:50 +0900

    (0528) Hello World

--- hello.c
+++ hello.c
@@ -2,5 +2,14 @@
 
 int main() {
     printf("Hello, world!\n");
+
+    int a = 100;
+
+    if (a<100){
+        printf("HaHaHa");
+    }
+
+
+
     return 0;
 }
---
+++ hello.js
@@ -0,0 +1,6 @@
+console.log("Hello, World!");
+
+let a = 100;
+if (a>99){
+    console.log("HaHaHa");
+}
