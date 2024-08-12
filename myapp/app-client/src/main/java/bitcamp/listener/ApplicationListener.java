8b60420883ec924048386984281ee95ff9f6c0f2
SA030 <suna_0132@naver.com>
2024-08-12 20:49:52 +0900

    ,

--- myapp/app-client/src/main/java/bitcamp/listener/ApplicationListener.java
+++ myapp/app-client/src/main/java/bitcamp/listener/ApplicationListener.java
@@ -6,7 +6,7 @@ import bitcamp.context.ApplicationContext;
 //
 public interface ApplicationListener {
 
-  void onStart(ApplicationContext ctx); // 애플리케이션이 시작될 때 호출됨
+  void onStart(ApplicationContext ctx) throws Exception; // 애플리케이션이 시작될 때 호출됨
 
   void onShutdown(ApplicationContext ctx); // 애플리케이션이 종료될 때 호출됨
 }
